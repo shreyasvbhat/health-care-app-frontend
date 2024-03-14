90b0666a7c3dada1854e3c5916e8b46907311835
01Sriharsha <harshavasu463@gmail.com>
2024-03-14 10:11:06 +0530

    starter code

---
+++ src/pages/home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='text-red-500'>Axios and React Router has been installed....</h1>
+    </div>
+  )
+}
