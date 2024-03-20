db6edda0344a247d485b49d3f388b3183f92efb3
01Sriharsha <harshavasu463@gmail.com>
2024-03-20 10:12:41 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
-import ProfilePage from "./pages/ProfilePage.jsx"
+import ProfilePage from "./pages/ProfilePage.jsx";
+import AuthenticatedRoute from "./components/AuthenticatedRoute.jsx";
+import AboutPage from "./pages/about.jsx";
+import Navbar from "./components/navbar-new.jsx";
 import LocationAccess from "./pages/LocationAccess.jsx";
+import WorkDetailsPage from "./pages/work-details.jsx";
+import NotFound from "./pages/not-found.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
+    <Routes>
+      {/* Public Routes */}
+      <Route path="/" element={<Navbar />}>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/profile" element={<ProfilePage/>} />
+        <Route path="/about" element={<AboutPage />} />
         <Route path="/location" element={<LocationAccess />} />
-      </Routes>
-    </BrowserRouter>
+      </Route>
+
+      {/* Authentication routes */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+
+      {/* Protected Routes */}
+      <Route element={<AuthenticatedRoute />}>
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/workdetails/:doctorId" element={<WorkDetailsPage />} />
+      </Route>
+
+      {/* Not Found Route */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
