e194dc1ff7b7a53c91706487d1a947eb57121dce
01Sriharsha <harshavasu463@gmail.com>
2024-03-22 18:05:45 +0530

    added doctor route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,3 @@
-export const publicRoutes = ["/", "/about", "/blogs" , "/search"];
+export const publicRoutes = ["/", "/about", "/blogs" , "/search" , "/doctor"];
 
 export const authRoutes = ["/login" , "/register"]
